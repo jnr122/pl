50b42478634f8e5633082c133865336286b0a400
jonah <jonah.n.rubin@gmail.com>
2020-04-24 16:45:30 -0400

    removing excess

--- src/Lang/L3.hs
+++
@@ -1,7 +0,0 @@
-module Lang.L3 
-  ( module Lang.L3.Data
-  , module Lang.L3.Util
-  ) where
-
-import Lang.L3.Data
-import Lang.L3.Util
--- src/Lang/L5.hs
+++
@@ -1,9 +0,0 @@
-module Lang.L5
-  ( module Lang.L5.Data
-  , module Lang.L5.Util
-  ) where
-
-import Lang.L5.Data
-import Lang.L5.Util
-
-
